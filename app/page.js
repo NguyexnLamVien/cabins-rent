1d38796bbacdad224bf4daf9960a2a64e5cbc68c
NguyexnLamVien <nlv.clone@gmail.com>
2025-09-06 18:50:43 +0700

    feat: add layout, loading, error, notFound, page, and icons

--- app/page.js
+++ app/page.js
@@ -1,11 +1,27 @@
+import Image from "next/image";
+import Link from "next/link";
+import bg from "@/public/bg.png";
 
-export default function Home() {
+export default function Page() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1 className="text-3xl sm:text-4xl font-bold text-center">
-        Welcome to Cabins Rent
-      </h1>
+    <main className="mt-24">
+      <Image src={bg} fill className="object-cover object-top"
+        // quality={80}
+        priority={true}
+        placeholder="blur"
+        alt="Mountains and forests with two cabins" />
 
-    </div>
+      <div className="relative z-10 text-center">
+        <h1 className="text-8xl text-primary-50 mb-10 tracking-tight font-normal">
+          Welcome to paradise.
+        </h1>
+        <Link
+          href="/cabins"
+          className="bg-accent-500 px-8 py-6 text-primary-800 text-lg font-semibold hover:bg-accent-600 transition-all"
+        >
+          Explore luxury cabins
+        </Link>
+      </div>
+    </main>
   );
 }
