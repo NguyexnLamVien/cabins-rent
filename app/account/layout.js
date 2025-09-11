a95f2af0a1d401b588c8010c8ff3e7e3de80598d
NguyexnLamVien <nlv.clone@gmail.com>
2025-09-11 16:42:43 +0700

    feat: account layout, page, loading

--- app/account/layout.js
+++ app/account/layout.js
@@ -1,7 +1,7 @@
 import SideNavigation from "../_components/SideNavigation";
 
 export default function Layout({ children }) {
-    return <div className="grid grid-cols-[16rem_1fr] h-full gap-12">
+    return <div className="grid grid-cols-[16rem_1fr] gap-12 h-screen">
         <SideNavigation />
         <div className="max-w-7xl mx-auto w-full" >{children}</div>
     </div>;
--- app/account/page.js
+++ app/account/page.js
@@ -1,8 +1,11 @@
+import { auth } from "../_lib/auth";
 
 export const metadata = {
   title: "Guest arena",
 }
 
-export default function Page() {
-  return <h1>Welcome, Your account</h1>;
+export default async function Page() {
+  const session = await auth();
+
+  return <h1>Welcome, {session.user.name}</h1>;
 }
